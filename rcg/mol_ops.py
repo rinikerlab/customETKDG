9a7f899c5c91f594157aa023c0de5df8ee7d55bf
hjuinj <shuzhe.wang1108@gmail.com>
2021-05-28 20:21:30 +0200

    handle stereochem after matching smiles to mol_block-type files

--- rcg/mol_ops.py
+++ rcg/mol_ops.py
@@ -11,10 +11,22 @@ from functools import reduce
 import difflib
 import logging
 from collections.abc import Iterable
+from rdkit.Geometry import Point3D
 
 logger = logging.getLogger(__name__)
 
 # logger.setLevel(logging.INFO)
+
+def load_coordinates(mol, np_array):
+    assert mol.GetNumAtoms() == len(np_array[1]), "number of atoms do not match"
+    init = mol.GetNumConformers()
+    for idx, coord in enumerate(np_array):
+        conf = Chem.Conformer(mol.GetNumAtoms())
+        for i in range(len(coord)):
+            x,y,z = coord[i]
+            conf.SetAtomPosition(i, Point3D(x,y,z))
+        mol.AddConformer(conf, init + idx)
+    return mol
 
 
 def _select(selection_string): #XXX try to use?
@@ -98,7 +110,7 @@ def assign_hydrogen_pdbinfo(mol, hydrogen_dict):
                     mi.SetResidueName(atm.GetPDBResidueInfo().GetResidueName())
                     mi.SetChainId(atm.GetPDBResidueInfo().GetChainId())
                     atm2.SetMonomerInfo(mi)
-    AllChem.Compute2DCoords(mol)
+    # AllChem.Compute2DCoords(mol) #XXX why this?
     return mol
 
 def assign_hydrogen_pdbinfo_blind(mol):
@@ -153,7 +165,7 @@ def assign_hydrogen_pdbinfo_blind(mol):
             mi.SetChainId(heavy_atm.GetPDBResidueInfo().GetChainId())
             atm.SetMonomerInfo(mi)
             logger.info("Residue number {}, hydrogen attached to atom {} is assigned name {}".format(heavy_atm.GetPDBResidueInfo().GetResidueNumber(), heavy_atm.GetPDBResidueInfo().GetName(), tmp)) #TODO make them into one INFO message
-    AllChem.Compute2DCoords(mol)
+    # AllChem.Compute2DCoords(mol) #XXX why this?
     return mol
 
 def hydrogen_dict_from_pdbmol(mol):
@@ -166,8 +178,43 @@ def hydrogen_dict_from_pdbmol(mol):
             out_dict.setdefault(atm.GetPDBResidueInfo().GetResidueNumber(), []).append(atm.GetPDBResidueInfo().GetName().strip())
     return out_dict
 
+def copy_stereo(from_mol, to_mol):
 
-def mol_from_smiles_pdb(smiles, pdb_filename, infer_names = True):
+    match = to_mol.GetSubstructMatch(from_mol)
+
+    # find chiral atoms:
+    chi_ats = []
+    for atom in from_mol.GetAtoms():
+        if atom.GetChiralTag() != Chem.ChiralType.CHI_UNSPECIFIED:
+            chi_ats.append(atom)
+
+    # copy over chiral info
+    for smi_atom in chi_ats:
+        # order of the neighbors around smi_atom
+        smi_order = [x.GetIdx() for x in smi_atom.GetNeighbors()]
+        
+        to_atom = to_mol.GetAtomWithIdx(match[smi_atom.GetIdx()])
+        to_atom.SetChiralTag(smi_atom.GetChiralTag())
+        
+        # check if we need to change that due to neighbor ordering differences
+        # current order of neighbors in the to_molol
+        mb_order = [x.GetIdx() for x in to_atom.GetNeighbors()]
+        
+        # with the smiles order:
+        mb_smi_order = [match[x] for x in smi_order]
+        
+        # check if it's a cyclic permutation:
+        tmp1 = ','.join([str(x) for x in mb_smi_order])
+        tmp2 = ','.join([str(x) for x in smi_order + smi_order])
+        
+        
+        if tmp1 not in tmp2:
+            # not cyclic:
+            to_atom.InvertChirality()
+    
+    return to_mol
+
+def mol_from_smiles_pdb(smiles, pdb_filename, infer_names = True, stereo_from_smiles = True):
     """
     Create rdkit mol from smiles string and pdb file
 
@@ -179,11 +226,15 @@ def mol_from_smiles_pdb(smiles, pdb_filename, infer_names = True):
         mol = Chem.MolFromPDBFile(pdb_filename, removeHs = True) 
         try:
             out_mol = AllChem.AssignBondOrdersFromTemplate(ref, mol)
+            
         except Exception as e:
             raise ValueError("Matching to pdb without hydrogens failed: {}".format(e))
 
-        out_mol = reorder_atoms(assign_hydrogen_pdbinfo_blind(Chem.AddHs(out_mol)))
+        out_mol = reorder_atoms(assign_hydrogen_pdbinfo_blind(Chem.AddHs(out_mol, addCoords = True)))
+
+        copy_stereo(ref, out_mol) if stereo_from_smiles else AllChem.AssignStereochemistryFrom3D(out_mol)
         return out_mol
+
     
     else:
         mol = Chem.MolFromPDBFile(pdb_filename, removeHs = False) 
@@ -202,13 +253,14 @@ def mol_from_smiles_pdb(smiles, pdb_filename, infer_names = True):
             raise ValueError("Matching to pdb without hydrogens failed: {}".format(e))
 
         if len(hydrogen_dict) == 0:
-            out_mol = reorder_atoms(assign_hydrogen_pdbinfo_blind(Chem.AddHs(out_mol)))
+            out_mol = reorder_atoms(assign_hydrogen_pdbinfo_blind(Chem.AddHs(out_mol, addCoords = True)))
         else:
             try:
-                out_mol = reorder_atoms(assign_hydrogen_pdbinfo(Chem.AddHs(out_mol), hydrogen_dict))
+                out_mol = reorder_atoms(assign_hydrogen_pdbinfo(Chem.AddHs(out_mol, addCoords = True), hydrogen_dict))
             except Exception as e:
                 logging.warning("\n {}. Reside to infering hydrogen names from their attached heavy atom names \n.".format(e))
-                out_mol = reorder_atoms(assign_hydrogen_pdbinfo_blind(Chem.AddHs(out_mol)))
+                out_mol = reorder_atoms(assign_hydrogen_pdbinfo_blind(Chem.AddHs(out_mol, addCoords = True)))
+        copy_stereo(ref, out_mol) if stereo_from_smiles else AllChem.AssignStereochemistryFrom3D(out_mol)
         return out_mol
 
 #TODO maybe better to place somewhere else?
@@ -261,7 +313,7 @@ def _get_overlap(s1, s2):
     the indices are comma-separated numbers in a string
 
     """
-    s = difflib.SequenceMatcher(None, s1, s2)
+    s = difflib.SequenceMatcher(None, s1, s2, autojunk=False)
     pos_a, pos_b, size = s.find_longest_match(0, len(s1), 0, len(s2)) 
     overlap = s1[pos_a:pos_a+size] 
     #partial index overlap can occur, limit to only overlap starting and ending with comma
@@ -271,6 +323,32 @@ def _get_overlap(s1, s2):
 
 
 
+# def get_rings(mol, accept_num_fused_atoms = 4):#TODO currently only pairwise ring merge, need to recursively check for ring merges
+#     """
+#     #shared (fused) part is always the smaller part of the ring, this way the whole ring needs to be at least 9 atoms
+#     """
+#     # for ring in mol.GetRingInfo().AtomRings():
+#     #     yield ring
+#     pre_ring_list_len = 0
+#     rings = [ring for ring in mol.GetRingInfo().AtomRings()]
+#     while len(rings) > pre_ring_list_len:
+#         tmp = [",".join([str(i) for i in list(r) * 3]) for r in rings[pre_ring_list_len:]]
+#         tmp_inverted = [",".join([str(i) for i in (list(r) * 3)[::-1]]) for r in rings[pre_ring_list_len:]]
+        
+#         pre_ring_list_len = len(rings)
+#         for i in range(len(tmp) - 1):
+#             for j in range(i+1, len(tmp)):
+#                 overlap = _get_overlap(tmp[i], tmp[j]).strip(",").split(",")
+#                 if len(overlap) > 0: #means there is at least some overlap, check inverting a string                
+#                     overlap2 = _get_overlap(tmp_inverted[i], tmp[j]).strip(",").split(",") 
+#                     if len(overlap) < len(overlap2):
+#                         overlap = overlap2
+#                         tmp_inverted[i], tmp[i] = tmp[i], tmp_inverted[i] #now i and j indices are in the same direction
+#                 if len(overlap) >= accept_num_fused_atoms:
+#                     part_a = tmp[j].split(",".join(overlap))[1] #split yields four sequences, one of the two middle pieces is always the complete subsequence
+#                     part_b = tmp_inverted[i].split(",".join(overlap[1:-1][::-1]))[1]
+#                     rings.append(tuple([int(i) for i in part_a.strip(",").split(",")] + [int(i) for i in part_b.strip(",").split(",")]))
+#     return rings
 def get_rings(mol, accept_num_fused_atoms = 4): 
     """
     #shared (fused) part is always the smaller part of the ring, this way the whole ring needs to be at least 9 atoms
